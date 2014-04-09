6e4d27dd8878322f67a1df6bcb203ef1c109dadc
BoppreH <boppreh@gmail.com>
2014-04-09 01:47:11 -0300

    Add docs

--- t.py
+++ t.py
@@ -2,6 +2,10 @@ from time import time
 import re
 
 class Task(object):
+    """
+    Class representing a single task. Its just a value store and is usually
+    paired with a TaskList that manages it.
+    """
     def __init__(self, id, name, start=None, end=None, is_active=False):
         self.id = id
         self.name = name
@@ -27,12 +31,22 @@ class Task(object):
                                           self.name)
 
 class TaskList(object):
+    """
+    List of tasks. Capable of loading and writing to files or strings.
+
+    Each task list creates tasks with locally unique ids and may have one task
+    set as "active".
+    """
     def __init__(self, tasks_by_id=None, active=None):
         self.tasks_by_id = tasks_by_id or {}
         self.active = active or None
         self.last_id = max(self.tasks_by_id) if len(self.tasks_by_id) else 0
 
     def create(self, task_name):
+        """
+        Creates and returns a new incomplete and inactive task with the given
+        name.
+        """
         self.last_id += 1
         task = Task(self.last_id,
                     task_name,
@@ -43,11 +57,17 @@ class TaskList(object):
         return task
 
     def destroy(self, task):
+        """
+        Removes the given task, deactivating it first if necessary.
+        """
         if task == self.active:
             self.deactivate()
         del self.tasks_by_id[task.id]
 
     def activate(self, task):
+        """
+        Replaces the previously active task with the given one.
+        """
         if self.active is not None:
             self.deactivate()
 
@@ -55,17 +75,30 @@ class TaskList(object):
         task.is_active = True
 
     def deactivate(self):
+        """
+        Deactivates the currently active task, leaving a None value. If there
+        is no active task, it does nothing.
+        """
         if self.active is not None:
             self.active.is_active = False
             self.active = None
 
     def finish(self, task):
+        """
+        Marks a task as finished.
+        """
         task.end = int(time())
 
     def __len__(self):
+        """
+        Returns the total number of tasks.
+        """
         return len(self.tasks_by_id)
 
     def __getitem__(self, id):
+        """
+        Returns a task by its id.
+        """
         return self.tasks_by_id[id]
 
     def __repr__(self):
@@ -75,17 +108,26 @@ class TaskList(object):
         return ''.join(result)
 
     def save(self, path):
+        """
+        Writes this task list to a file.
+        """
         with open(path, 'w') as f:
             f.write(str(self).encode('utf-8'))
 
     @staticmethod
     def load(path):
+        """
+        Loads a task list from a file.
+        """
         with open(path, 'r') as f:
             text = f.read()
         return TaskList.parse(text)
 
     @staticmethod
     def parse(string):
+        """
+        Creates a task list from the given serialized string.
+        """
         active = None
         tasks_by_id = {}
 
